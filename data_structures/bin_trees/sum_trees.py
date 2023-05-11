ce84de8d8cf7a902d3daf19a8bb469cacb9e040c
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-05-11 07:07:14 -0500

    tree sum implemented using Breadth First Search with a queue

--- data_structures/bin_trees/sum_trees.py
+++ data_structures/bin_trees/sum_trees.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def sum_tree(root):
     if root is None:
         return 0
@@ -7,7 +10,7 @@ def sum_tree(root):
 
     return root.val + left_sum + right_sum
 
-def sum_tree_bfs(root):
+def sum_tree_dfs(root):
     if root is None:
         return 0
     
@@ -20,8 +23,23 @@ def sum_tree_bfs(root):
 
         for child in (curr.right, curr.left):
             if child is not None:
-                stack.append(child)
+                stack.append(child)    
+    return total_sum
+
+def sum_tree_bfs(root):
+    if root is None:
+        return 0
     
+    total_sum = 0
+
+    queue = deque( [ root ] )
+    while queue:
+        curr = queue.popleft()
+        total_sum += curr.val
+
+        for child in (curr.left, curr.right):
+            if child is not None:
+                queue.append(child)
     return total_sum
 
 class Node:
@@ -33,4 +51,5 @@ class Node:
 bin_tree = Node(2, Node(5, Node(100), Node(200)), Node(10, right=Node(1000)))
 
 print(f"{sum_tree(root=bin_tree) = }")
+print(f"{sum_tree_dfs(root=bin_tree) = }")
 print(f"{sum_tree_bfs(root=bin_tree) = }")
