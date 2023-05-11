71d28c86149e1564083f200783d9413aa9128082
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-05-11 07:01:39 -0500

    sum_tree implemented using Depth First Search iterative using a list as a stack.

--- data_structures/bin_trees/sum_trees.py
+++ data_structures/bin_trees/sum_trees.py
@@ -7,6 +7,23 @@ def sum_tree(root):
 
     return root.val + left_sum + right_sum
 
+def sum_tree_bfs(root):
+    if root is None:
+        return 0
+    
+    total_sum = 0
+
+    stack = [ root ]
+    while stack:
+        curr = stack.pop()
+        total_sum += curr.val
+
+        for child in (curr.right, curr.left):
+            if child is not None:
+                stack.append(child)
+    
+    return total_sum
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -16,3 +33,4 @@ class Node:
 bin_tree = Node(2, Node(5, Node(100), Node(200)), Node(10, right=Node(1000)))
 
 print(f"{sum_tree(root=bin_tree) = }")
+print(f"{sum_tree_bfs(root=bin_tree) = }")
