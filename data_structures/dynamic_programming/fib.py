69c9f94a2a0e03e4ab5df8706ca84d3207cc809c
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-05-13 07:04:28 -0500

    classic fib with the memo dictionary

---
+++ data_structures/dynamic_programming/fib.py
@@ -0,0 +1,18 @@
+def fib(n):
+    return _fib(n, memo={})
+
+def _fib(n, memo):
+    if n in memo:
+        return memo[n]
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    memo[n] =  _fib(n-1, memo) + _fib(n-2, memo)
+    return memo[n]
+
+
+for _ in range(35):
+    print(f"{_}-->{fib(n=_)}")
