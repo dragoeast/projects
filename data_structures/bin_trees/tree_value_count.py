e04c23e032e60b2d0b6ef9cc4f3583ef6de47979
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-05-14 07:40:28 -0500

    depth first search iterative implementation added

--- data_structures/bin_trees/tree_value_count.py
+++ data_structures/bin_trees/tree_value_count.py
@@ -1,3 +1,20 @@
+def tree_value_count_iterative(root, target):
+    if root is None:
+        return 0
+    
+    counter = 0
+    stack = [ root ]
+    while stack:
+        curr = stack.pop()
+        if curr.val == target:
+            counter += 1
+        
+        for child in (curr.right, curr.left):
+            if child is not None:
+                stack.append(child)
+
+    return counter
+
 def tree_value_count(root, target):
     if root is None:
         return 0
@@ -18,3 +35,4 @@ class Node:
 binary_tree = Node(2, Node(5, Node(4), Node(5)), Node(6, right=Node(5)))
 
 print(f"{tree_value_count(root=binary_tree, target=5) = }")
+print(f"{tree_value_count_iterative(root=binary_tree, target=5) = }")
